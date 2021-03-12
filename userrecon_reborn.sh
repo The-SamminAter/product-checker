9039283f31ff3af570ed485501b9f7f1516b4f58
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2021-03-11 17:41:17 -0800

    Continued work, not functional yet

--- userrecon_reborn.sh
+++ userrecon_reborn.sh
@@ -1,44 +1,107 @@
 #!/bin/bash
-RED="\e[1;91m"
-GRN="\e[1;92m"
-BLU="\e[1;77m" #used to be 0;94
+
+#Coloring:
 RST="\e[0;0m"
+RED="\e[1;91m"
+GRN="\e[1;92m" #1;32 or 1;92
+BLU="\e[1;96m" #What's supposed to be blue looks purple so cyan (1;36 or 1;96) is used
+YLW="\e[1;93m" #1;33 or 1;93
 
-#Title; sorry about the general messiness, I've tried to keep it relatively clean
-printf "${RED}\n"
-printf "                                                   .-\"\"\"\"-.	\n" #cheap trick to show four quotation marks
-printf "                                                  /        \	\n"
-printf "${GRN}  _   _               ____                      ${RED} /_        _\	\n"
-printf "${GRN} | | | |___  ___ _ __|  _ \ ___  ___ ___  _ __  ${RED}// \      / \\"; echo "\\" #cheap trick to show two back-slashes
-printf "${GRN} | | | / __|/ _ \ '__| |_) / _ \/ __/ _ \| '_ \ ${RED}|\__\    /__/|	\n"
-printf "${GRN} | |_| \__ \  __/ |  |  _ <  __/ (_| (_) | | | |${RED} \    ||    /	\n"
-printf "${GRN}  \___/|___/\___|_|  |_| \_\___|\___\___/|_| |_|${RED}  \        / 	\n"
-printf " Reborn ----------------------------------- v0.0   \  __  /		\n"
-printf "                                                    '.__.'		\n"
-printf "${BLU}"
+#If the user or the script exits (while not being ran in silent mode), this clears the colors
+exitRST()
+{
+	if [ "${silent}" != true ]
+	then
+		printf "${RST}"
+	fi
+	exit
+}
+trap "exitRST" EXIT 
 
-#Username handling
-name=""
-if [ "$1" != "" ]
+#Checks if $1 is an argument or not, mainly for use in the elif in name and argument handling
+if [ $(printf '%s' "$1" | cut -c1) == "-" ]
 then
-	name="$1"
-	echo "Using ${name} as the username"
+	if [ "$1" == "-s" ] || [ "$1" == "--silent" ] || [ "$1" == "-y" ] || [ "$1" == "--yes" ] || [ "$1" == "-n" ] || [ "$1" == "--no" ]
+	then
+		isArg=true
+	else
+		isArg=false
+	fi
+else
+	isArg=false
 fi
-while [ "${name}" == "" ]
-do
-	read -p "Username: " name
-	#Add similar name(s) option
-done
+
+#Name and argument handling
+name=""
+silent=false
+if [[ "${isArg}" == true && "$1" == "-s" ]] || [[ "${isArg}" == true && "$1" == "--silent" ]] #Silent mode
+then
+	silent=true
+	if [ "$2" ]
+	then
+		name="$2"
+		fCreate=true
+	else
+		#I figure that if this is being ran in silent mode, then it doesn't need color
+		printf "[!] Error: while using silent mode, arg 2 can't be empty\n"
+		exit
+	fi
+elif [ $1 ] && [ "${isArg}" == false ] #For if the script was launched with a name as an argument; doesn't currently work
+then
+	printf "\e[40m" #Set a black background
+	name="$1"
+	if [ "$2" == "-y" ] || [ "$2" == "--yes" ]
+	then
+		fCreate=true
+	elif [ "$2" == "-n" ] || [ "$2" == "--no" ]
+	then
+		fCreate=false
+	fi
+	printf "${BLU}Using ${name} as the username\n"
+else
+	printf "\e[40m" #Set a black background
+	if [ "$1" == "-y" ] || [ "$1" == "--yes" ]
+	then
+		fCreate=true
+	elif [ "$1" == "-n" ] || [ "$1" == "--no" ]
+	then
+		fCreate=false
+	fi
+	#Title; sorry about the general messiness, I've tried to keep it relatively clean
+	#printf "${RED}\n"
+	printf "${RED}                                                   .-\"\"\"\"-.	\n" #cheap trick to show four quotation marks
+	printf "                                                  /        \	\n"
+	printf "${GRN}  _   _               ____                      ${RED} /_        _\	\n"
+	printf "${GRN} | | | |___  ___ _ __|  _ \ ___  ___ ___  _ __  ${RED}// \      / \\"; echo "\\" #cheap trick to show two back-slashes
+	printf "${GRN} | | | / __|/ _ \ '__| |_) / _ \/ __/ _ \| '_ \ ${RED}|\__\    /__/|	\n"
+	printf "${GRN} | |_| \__ \  __/ |  |  _ <  __/ (_| (_) | | | |${RED} \    ||    /	\n"
+	printf "${GRN}  \___/|___/\___|_|  |_| \_\___|\___\___/|_| |_|${RED}  \        / 	\n"
+	printf "${BLU} Reborn ${RED}----------------------------------- ${BLU}v0.0${RED}   \  __  /		\n"
+	printf "                                                    '.__.'		\n"
+	while [ "${name}" == "" ]
+	do
+		printf "${BLU}[${YLW}?${BLU}] ${YLW}Username:${RED}"
+		read -p " " name
+		#Add similar name(s) option; for example
+	done
+fi
 
 #File handling
-#Possibly change (fCreate) in the future to allow the use of direct input (think of through something like $2/--fCreate=true)
-fCreate=false
-fCreateRaw=""
-#read -p "Do you want to save the output to a file? " fCreateRaw
-if [ "${fCreateRaw}" == "y" ] || [ "${fCreateRaw}" == "Y" ] || [ "${fCreateRaw}" == "Yes" ] || [ "${fCreateRaw}" == "yes" ] #I know, it's crude, but it works
+if [ ${fCreate} ] #If the variable exists
 then
-	fCreate=true
-	#fCount (and fName) are in place to prevent overwriting or adding on to a pre-existing file (if the user chooses to write it)
+	printf ""
+else
+	fCreate=false
+	fCreateRaw=""
+	read -p "Do you want to save the output to a file? " fCreateRaw
+	if [ "${fCreateRaw}" == "y" ] || [ "${fCreateRaw}" == "Y" ] || [ "${fCreateRaw}" == "Yes" ] || [ "${fCreateRaw}" == "yes" ] #I know, it's crude, but it works
+	then
+		fCreate=true
+	fi
+fi
+if [ "${fCreate}" == true ]
+then
+	#fCount (and fName) are in place to prevent overwriting or adding on to a pre-existing file
 	fCount=1
 	fName="${name}-${fCount}.txt"
 	while [ -f "./${fName}" ]
@@ -46,7 +109,7 @@ then
 		let "fCount++"
 		fName="${name}-${fCount}.txt"
 	done
-	touch "${fName}"
+	#touch "${fName}"
 fi
 
 #scan() scans URLs based on info passed to them, and outputs the results
@@ -71,7 +134,7 @@ scan()
 	elif [ "${cleanedResult}" == "" ]
 	then
 		#printf="${BLU}[${RED}X${BLU}] ${RED}Failure: ${BLU}$1 ${RED}not found\n"
-		status="failure"
+		status="failure: scan()"
 	fi
 	echo "${status}"
 	print 
@@ -81,11 +144,8 @@ scan()
 #print() prints and writes (to fName) information passed to it by scan()
 print()
 {
-
+	echo "stub: print()"
 }
 
 #Reddit
-scan "https://api.reddit.com/users/${username}" "Reddit"
-
-#End of script, reset colors
-printf "${RST}"
+#scan "https://api.reddit.com/users/${username}" "Reddit"
