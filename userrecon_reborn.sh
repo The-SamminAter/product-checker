5e23de21e5d6792f203d44ad4cb8109bfc307d8b
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2021-03-16 15:55:30 -0700

    It works!

--- userrecon_reborn.sh
+++ userrecon_reborn.sh
@@ -22,7 +22,7 @@ trap "exitRST" EXIT
 
 
 #Checks if $1 is an argument or not, mainly for use in the elif in name and argument handling
-if [ $(printf '%s' "$1" | cut -c1) == "-" ]
+if [[ $(printf '%s' "$1" | cut -c1) == "-" ]]
 then
 	if [ "$1" == "-s" ] || [ "$1" == "--silent" ] || [ "$1" == "-y" ] || [ "$1" == "--yes" ] || [ "$1" == "-n" ] || [ "$1" == "--no" ]
 	then
@@ -41,18 +41,19 @@ silent=false
 if [[ "${isArg}" == true && "$1" == "-s" ]] || [[ "${isArg}" == true && "$1" == "--silent" ]] #Silent mode
 then
 	silent=true
-	if [ "$2" ]
+	if [ $2 ]
 	then
 		name="$2"
 		fCreate=true
 	else
 		#I figure that if this is being ran in silent mode, then it doesn't need color
-		printf "[!] Error: while using silent mode, arg 2 can't be empty\n"
+		printf "[!] Error: while using silent mode, arg 2 can't be empty\n" #I can't have this printed to the file, because it hasn't been made yet
 		exit
 	fi
 elif [ $1 ] && [ "${isArg}" == false ] #For if the script was launched with a name as an argument; doesn't currently work
 then
-	printf "\e[40m" #Set a black background
+	#Setting the background turned out to be iffy, so I'm just not doing it
+	#printf "\e[40m" #Set a black background
 	name="$1"
 	if [ "$2" == "-y" ] || [ "$2" == "--yes" ]
 	then
@@ -61,9 +62,9 @@ then
 	then
 		fCreate=false
 	fi
-	printf "${BLU}Using ${name} as the username\n"
 else
-	printf "\e[40m" #Set a black background
+	#Setting the background turned out to be iffy, so I'm just not doing it
+	#printf "\e[40m" #Set a black background
 	if [ "$1" == "-y" ] || [ "$1" == "--yes" ]
 	then
 		fCreate=true
@@ -73,20 +74,20 @@ else
 	fi
 	#Title; sorry about the general messiness, I've tried to keep it relatively clean
 	#printf "${RED}\n"
-	printf "${RED}                                                   .-\"\"\"\"-.	\n" #cheap trick to show four quotation marks
+	printf "${RED}                                                   .-\"\"\"\"-.	\n"; #trick to show four quotation marks
 	printf "                                                  /        \	\n"
 	printf "${GRN}  _   _               ____                      ${RED} /_        _\	\n"
-	printf "${GRN} | | | |___  ___ _ __|  _ \ ___  ___ ___  _ __  ${RED}// \      / \\"; echo "\\" #cheap trick to show two back-slashes
+	printf "${GRN} | | | |___  ___ _ __|  _ \ ___  ___ ___  _ __  ${RED}// \      / \\"; echo "\\	" #cheap trick to show two back-slashes
 	printf "${GRN} | | | / __|/ _ \ '__| |_) / _ \/ __/ _ \| '_ \ ${RED}|\__\    /__/|	\n"
 	printf "${GRN} | |_| \__ \  __/ |  |  _ <  __/ (_| (_) | | | |${RED} \    ||    /	\n"
 	printf "${GRN}  \___/|___/\___|_|  |_| \_\___|\___\___/|_| |_|${RED}  \        / 	\n"
-	printf "${BLU} Reborn ${RED}----------------------------------- ${BLU}v0.0${RED}   \  __  /		\n"
-	printf "                                                    '.__.'		\n"
+	printf "${BLU} Reborn ${RED}----------------------------------- ${BLU}v0.0${RED}   \  __  /	\n"
+	printf "                                                    '.__.'	\n"
 	while [ "${name}" == "" ]
 	do
-		printf "${BLU}[${YLW}?${BLU}] ${YLW}Username:${RED}"
-		read -p " " name
-		#Add similar name(s) option; for example
+		printf "${BLU}[?] Username: "
+		read -p "" name
+		#Add similar name(s) option
 	done
 fi
 
@@ -98,7 +99,8 @@ then
 else
 	fCreate=false
 	fCreateRaw=""
-	read -p "Do you want to save the output to a file? " fCreateRaw
+	printf "${BLU}[?] Do you want to save the output to a file: "
+	read -p "" fCreateRaw
 	if [ "${fCreateRaw}" == "y" ] || [ "${fCreateRaw}" == "Y" ] || [ "${fCreateRaw}" == "Yes" ] || [ "${fCreateRaw}" == "yes" ] #I know, it's crude, but it works
 	then
 		fCreate=true
@@ -118,32 +120,41 @@ then
 fi
 
 
+#This just lets the user know that the script is working, if silent mode isn't enabled
+if [ "${silent}" == false ]
+then
+	printf "${BLU}[*] Checking for ${name}\n"
+fi
+
+
 #scan() scans URLs based on info passed to them, and outputs the results
 #This was made with the assumption that if a name/page doesn't exist, it (the username) won't be mentioned anywhere in the curl'd text
 #Also, the output (from scan()) is to be colored
 scan()
 {
 	#Thought of/planned inputs
-	#$1: Name (of site)
+	#$1: name (of site)
 	#$2: URL
 	#$3: success text/message
 	#$4: failure text/message (if necessary) (not yet written)
 	#$5: additional error text/message (if necessary) (not yet written)
-	status="If you see this, something is wrong"
+	#
+	#This should be re-organized to curl before the if statement, and then the arguments/their existence dictates what happens
+	#That way, errors will be recognized and retained, instead of the status just being "failure"
+	#
 	if [ "$4" ] || [ "$5" ]
 	then
-		echo "stub: scan(): if arg4 or arg5 exist"
+		echo "stub: scan(): arg4 or arg5 exist"
 	else
 		result=$(curl -s -A "UserRecon Reborn/0.0" "$2" | grep "$3") #Custom user agent
-		#if [ "${result}" != "" ] #Pretty much, if the result isn't nothing, we presume that it exists
-		if [ -n "${result}" ]
+		if [ -n "${result}" ] #If $result isn't null, we presume success. For when this isn't the case, use $4 and $5
 		then
-			status="success $1"
+			status="success"
 		else
-			status="failure $1"
+			status="failure"
 		fi
 	fi
-	print "${status}"
+	print "${status}" "$1"
 	unset status #Unsets the variable, so it doesn't exist until scan() gets called again
 }
 
@@ -151,7 +162,37 @@ scan()
 #print() prints and writes (to fName) information passed to it by scan()
 print()
 {
-	echo "stub: print()"
+	#$1: status message
+	#$2: site name/error message (if $1 is "error")
+	#
+	#Printing out the results (to the console) if the script isn't being ran in silent mode
+	if [ "${silent}" == false ]
+	then
+		if [ "$1" == "success" ]
+		then
+			printf "${BLU}[${GRN}\xE2\x9C\x94${BLU}] ${GRN}$2 found\n"
+		elif [ "$1" == "failure" ]
+		then
+			printf "${BLU}[${RED}X${BLU}] ${RED}$2 not found\n"
+		elif [ "$1" == "error" ]
+		then
+			printf "${BLU}[${YLW}!${BLU}] ${YLW}Error: $2\n"
+		fi
+	fi
+	#Printing out the results (to fName) if the user wants a file created
+	if [ "${fCreate}" == true ]
+	then
+		if [ "$1" == "success" ]
+		then
+			printf "[\xE2\x9C\x94] $2 found\n" >> "${fName}"
+		elif [ "$1" == "failure" ]
+		then
+			printf "[X] $2 not found\n" >> "${fName}"
+		elif [ "$1" == "error" ]
+		then
+			printf "[!] Error: $2\n" >> "${fName}"
+		fi
+	fi
 }
 
 
